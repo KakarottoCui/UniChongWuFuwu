d66735fdff32a2d2f3a420f9d46271795ad941a2
KakarottoCui <471363336@qq.com>
2025-05-21 16:43:43 +0800

    Update index.config.js

--- config/index.config.js
+++ config/index.config.js
@@ -7,7 +7,7 @@ const CONFIG = {
 			baseUrl: 'https://izhaochong.noooya.com/api', // 后台接口请求地址
 			hostUrl: 'https://izhaochong.noooya.com/h5', // H5地址(前端运行地址)
 	    websocketUrl: '', // websocket服务端地址
-	    weixinAppId: 'wxc4bbbe1b3b1b4a71' // 微信公众号appid
+	    weixinAppId: '' // 微信公众号appid
 	},
 	// 生产环境配置
 	production: {
@@ -16,7 +16,7 @@ const CONFIG = {
 	    baseUrl: 'https://izhaochong.noooya.com/api', // 后台接口请求地址
 	    hostUrl: 'https://izhaochong.noooya.com/h5', // H5地址(前端运行地址)
 	    websocketUrl: '', // websocket服务端地址
-	    weixinAppId: 'wxc4bbbe1b3b1b4a71' // 微信公众号appid
+	    weixinAppId: '' // 微信公众号appid
 	}
 
 };
